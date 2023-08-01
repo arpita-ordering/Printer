c65a5fd9cdd6f551929abbe9c7c5ac89a045c21a
arpita-ordering <97442051+arpita-ordering@users.noreply.github.com>
2023-08-01 21:24:22 +0530

    add new code
    add new code

--- src/components/OrderSummary/index.tsx
+++ src/components/OrderSummary/index.tsx
@@ -345,149 +345,149 @@ const interfaceTypes: Array<InterfaceType> = [InterfaceType.Bluetooth]
   // @NOTE iOS Only
   const silentPrint = async () => {
     console.log("call printer function")
-  //   alert("1"+interfaceTypes)
+    alert("1"+interfaceTypes)
 
-  //   // var settings = new StarConnectionSettings();
-  //   // settings.interfaceType = state.interfaceType;
-  //   // settings.identifier = state.identifier;
-  //   // settings.autoSwitchInterface = true;
+    // var settings = new StarConnectionSettings();
+    // settings.interfaceType = state.interfaceType;
+    // settings.identifier = state.identifier;
+    // settings.autoSwitchInterface = true;
 
-  //   // If you are using Android 12 and targetSdkVersion is 31 or later,
-  //   // you have to request Bluetooth permission (Nearby devices permission) to use the Bluetooth printer.
-  //   // https://developer.android.com/about/versions/12/features/bluetooth-permissions
-  //   if (Platform.OS == 'android' && 31 <= Platform.Version) {
-  //       if (state.bluetoothIsEnabled ) {
-  //           var hasPermission = _confirmBluetoothPermission();
+    // If you are using Android 12 and targetSdkVersion is 31 or later,
+    // you have to request Bluetooth permission (Nearby devices permission) to use the Bluetooth printer.
+    // https://developer.android.com/about/versions/12/features/bluetooth-permissions
+    if (Platform.OS == 'android' && 31 <= Platform.Version) {
+        if (state.bluetoothIsEnabled ) {
+            var hasPermission = _confirmBluetoothPermission();
             
-  //           if (!hasPermission) {
-  //               console.log(`PERMISSION ERROR: You have to allow Nearby devices to use the Bluetooth printer`);
-  //               return;
-  //           }
-  //       }
-  //   }
+            if (!hasPermission) {
+                console.log(`PERMISSION ERROR: You have to allow Nearby devices to use the Bluetooth printer`);
+                return;
+            }
+        }
+    }
     
 
-  //   try {
-  //     await mStarDeviceDiscoveryManager.stopDiscovery()
+    try {
+      await mStarDeviceDiscoveryManager.stopDiscovery()
 
-  //     // var interfaceTypes: Array<InterfaceType> = []
-  //     // if(state.lanIsEnabled) {
-  //     //     interfaceTypes.push(InterfaceType.Lan);
-  //     // }
-  //     // if(state.bluetoothIsEnabled) {
-  //     //     interfaceTypes.push(InterfaceType.Bluetooth);
-  //     // }
-  //     // if(state.bluetoothLeIsEnabled) {
-  //     //     interfaceTypes.push(InterfaceType.BluetoothLE);
-  //     // }
-  //     // if(state.usbIsEnabled) {
-  //     //     interfaceTypes.push(InterfaceType.Usb);
-  //     // }
+      // var interfaceTypes: Array<InterfaceType> = []
+      // if(state.lanIsEnabled) {
+      //     interfaceTypes.push(InterfaceType.Lan);
+      // }
+      // if(state.bluetoothIsEnabled) {
+      //     interfaceTypes.push(InterfaceType.Bluetooth);
+      // }
+      // if(state.bluetoothLeIsEnabled) {
+      //     interfaceTypes.push(InterfaceType.BluetoothLE);
+      // }
+      // if(state.usbIsEnabled) {
+      //     interfaceTypes.push(InterfaceType.Usb);
+      // }
 
-  //     mStarDeviceDiscoveryManager = await StarDeviceDiscoveryManagerFactory.create(interfaceTypes);
-  //     mStarDeviceDiscoveryManager.discoveryTime = 10000;
+      mStarDeviceDiscoveryManager = await StarDeviceDiscoveryManagerFactory.create(interfaceTypes);
+      mStarDeviceDiscoveryManager.discoveryTime = 10000;
 
-  //     mStarDeviceDiscoveryManager.onPrinterFound = (printer: StarPrinter) => {
-  //         const printers = state.printers;
-  //         printers.push(printer);
-  //         setState({printers: printers});
+      mStarDeviceDiscoveryManager.onPrinterFound = (printer: StarPrinter) => {
+          const printers = state.printers;
+          printers.push(printer);
+          setState({printers: printers});
 
-  //         console.log(`Found printer: ${printer.connectionSettings.identifier}.`);
-  //     };
+          console.log(`Found printer: ${printer.connectionSettings.identifier}.`);
+      };
 
-  //     mStarDeviceDiscoveryManager.onDiscoveryFinished = () => {
-  //         console.log(`Discovery finished.`);
-  //     };
+      mStarDeviceDiscoveryManager.onDiscoveryFinished = () => {
+          console.log(`Discovery finished.`);
+      };
 
-  //     await mStarDeviceDiscoveryManager.startDiscovery();
-  // }
-  // catch(error) {
-  //     console.log(`Error: ${String(error)}`);
-  //     alert("Error===>"+error)
-  // }
+      await mStarDeviceDiscoveryManager.startDiscovery();
+  }
+  catch(error) {
+      console.log(`Error: ${String(error)}`);
+      alert("Error===>"+error)
+  }
 
-    // var printer = new StarPrinter(settings);
+    var printer = new StarPrinter(settings);
 
-    // try {
-    //     var builder = new StarXpandCommand.StarXpandCommandBuilder();
-    //     builder.addDocument(new StarXpandCommand.DocumentBuilder()
-    //     .addPrinter(new StarXpandCommand.PrinterBuilder()
-    //         .actionPrintImage(new StarXpandCommand.Printer.ImageParameter("logo_01.png", 406))
-    //         .styleInternationalCharacter(StarXpandCommand.Printer.InternationalCharacterType.Usa)
-    //         .styleCharacterSpace(0)
-    //         .styleAlignment(StarXpandCommand.Printer.Alignment.Center)
-    //         .actionPrintText("Star Clothing Boutique\n" +
-    //                         "123 Star Road\n" +
-    //                         "City, State 12345\n" +
-    //                         "\n")
-    //         .styleAlignment(StarXpandCommand.Printer.Alignment.Left)
-    //         .actionPrintText("Date:MM/DD/YYYY    Time:HH:MM PM\n" +
-    //                         "--------------------------------\n" +
-    //                         "\n")
-    //         .actionPrintText("SKU         Description    Total\n" +
-    //                         "300678566   PLAIN T-SHIRT  10.99\n" +
-    //                         "300692003   BLACK DENIM    29.99\n" +
-    //                         "300651148   BLUE DENIM     29.99\n" +
-    //                         "300642980   STRIPED DRESS  49.99\n" +
-    //                         "300638471   BLACK BOOTS    35.99\n" +
-    //                         "\n" +
-    //                         "Subtotal                  156.95\n" +
-    //                         "Tax                         0.00\n" +
-    //                         "--------------------------------\n")
-    //         .actionPrintText("Total     ")
-    //         .add(new StarXpandCommand.PrinterBuilder()
-    //             .styleMagnification(new StarXpandCommand.MagnificationParameter(2, 2))
-    //             .actionPrintText("   $156.95\n")
-    //         )
-    //         .actionPrintText("--------------------------------\n" +
-    //                         "\n" +
-    //                         "Charge\n" +
-    //                         "156.95\n" +
-    //                         "Visa XXXX-XXXX-XXXX-0123\n" +
-    //                         "\n")
-    //         .add(new StarXpandCommand.PrinterBuilder()
-    //             .styleInvert(true)
-    //             .actionPrintText("Refunds and Exchanges\n")
-    //         )
-    //         .actionPrintText("Within ")
-    //         .add(new StarXpandCommand.PrinterBuilder()
-    //             .styleUnderLine(true)
-    //             .actionPrintText("30 days")
-    //         )
-    //         .actionPrintText(" with receipt\n")
-    //         .actionPrintText("And tags attached\n" +
-    //                         "\n")
-    //         .styleAlignment(StarXpandCommand.Printer.Alignment.Center)
-    //         .actionPrintBarcode(new StarXpandCommand.Printer.BarcodeParameter('0123456',
-    //                             StarXpandCommand.Printer.BarcodeSymbology.Jan8)
-    //                             .setBarDots(3)
-    //                             .setBarRatioLevel(StarXpandCommand.Printer.BarcodeBarRatioLevel.Level0)
-    //                             .setHeight(5)
-    //                             .setPrintHri(true))
-    //         .actionFeedLine(1)
-    //         .actionPrintQRCode(new StarXpandCommand.Printer.QRCodeParameter('Hello World.\n')
-    //                             .setModel(StarXpandCommand.Printer.QRCodeModel.Model2)
-    //                             .setLevel(StarXpandCommand.Printer.QRCodeLevel.L)
-    //                             .setCellSize(8))
-    //         .actionCut(StarXpandCommand.Printer.CutType.Partial)
-    //         )
-    //     );
+    try {
+        var builder = new StarXpandCommand.StarXpandCommandBuilder();
+        builder.addDocument(new StarXpandCommand.DocumentBuilder()
+        .addPrinter(new StarXpandCommand.PrinterBuilder()
+            .actionPrintImage(new StarXpandCommand.Printer.ImageParameter("logo_01.png", 406))
+            .styleInternationalCharacter(StarXpandCommand.Printer.InternationalCharacterType.Usa)
+            .styleCharacterSpace(0)
+            .styleAlignment(StarXpandCommand.Printer.Alignment.Center)
+            .actionPrintText("Star Clothing Boutique\n" +
+                            "123 Star Road\n" +
+                            "City, State 12345\n" +
+                            "\n")
+            .styleAlignment(StarXpandCommand.Printer.Alignment.Left)
+            .actionPrintText("Date:MM/DD/YYYY    Time:HH:MM PM\n" +
+                            "--------------------------------\n" +
+                            "\n")
+            .actionPrintText("SKU         Description    Total\n" +
+                            "300678566   PLAIN T-SHIRT  10.99\n" +
+                            "300692003   BLACK DENIM    29.99\n" +
+                            "300651148   BLUE DENIM     29.99\n" +
+                            "300642980   STRIPED DRESS  49.99\n" +
+                            "300638471   BLACK BOOTS    35.99\n" +
+                            "\n" +
+                            "Subtotal                  156.95\n" +
+                            "Tax                         0.00\n" +
+                            "--------------------------------\n")
+            .actionPrintText("Total     ")
+            .add(new StarXpandCommand.PrinterBuilder()
+                .styleMagnification(new StarXpandCommand.MagnificationParameter(2, 2))
+                .actionPrintText("   $156.95\n")
+            )
+            .actionPrintText("--------------------------------\n" +
+                            "\n" +
+                            "Charge\n" +
+                            "156.95\n" +
+                            "Visa XXXX-XXXX-XXXX-0123\n" +
+                            "\n")
+            .add(new StarXpandCommand.PrinterBuilder()
+                .styleInvert(true)
+                .actionPrintText("Refunds and Exchanges\n")
+            )
+            .actionPrintText("Within ")
+            .add(new StarXpandCommand.PrinterBuilder()
+                .styleUnderLine(true)
+                .actionPrintText("30 days")
+            )
+            .actionPrintText(" with receipt\n")
+            .actionPrintText("And tags attached\n" +
+                            "\n")
+            .styleAlignment(StarXpandCommand.Printer.Alignment.Center)
+            .actionPrintBarcode(new StarXpandCommand.Printer.BarcodeParameter('0123456',
+                                StarXpandCommand.Printer.BarcodeSymbology.Jan8)
+                                .setBarDots(3)
+                                .setBarRatioLevel(StarXpandCommand.Printer.BarcodeBarRatioLevel.Level0)
+                                .setHeight(5)
+                                .setPrintHri(true))
+            .actionFeedLine(1)
+            .actionPrintQRCode(new StarXpandCommand.Printer.QRCodeParameter('Hello World.\n')
+                                .setModel(StarXpandCommand.Printer.QRCodeModel.Model2)
+                                .setLevel(StarXpandCommand.Printer.QRCodeLevel.L)
+                                .setCellSize(8))
+            .actionCut(StarXpandCommand.Printer.CutType.Partial)
+            )
+        );
 
-    //     var commands = await builder.getCommands();
+        var commands = await builder.getCommands();
 
-    //     await printer.open();
-    //     await printer.print(commands);
+        await printer.open();
+        await printer.print(commands);
 
-    //     console.log(`Success`);
-    // }
-    // catch(error) {
-    //   alert(`Error: ${String(error)}`)
-    //     console.log(`Error: ${String(error)}`);
-    // }
-    // finally {
-    //     await printer.close();
-    //     await printer.dispose();
-    // }
+        console.log(`Success`);
+    }
+    catch(error) {
+      alert(`Error: ${String(error)}`)
+        console.log(`Error: ${String(error)}`);
+    }
+    finally {
+        await printer.close();
+        await printer.dispose();
+    }
     // if (!state?.selectedPrinter) {
     //   Alert.alert('Must Select Printer First');
     // }
